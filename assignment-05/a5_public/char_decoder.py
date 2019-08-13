931e6f33d09fc3756716d6385682e83a9a004bcf
Brycexxx <2531094755@qq.com>
2019-08-13 20:56:18 +0800

    精度还是不够啊:weary:

--- assignment-05/a5_public/char_decoder.py
+++ assignment-05/a5_public/char_decoder.py
@@ -7,6 +7,13 @@ CS224N 2018-19: Homework 5
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
+from utils import LOGLEVEL
+import logging
+
+logging.basicConfig(level=LOGLEVEL, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 class CharDecoder(nn.Module):
     def __init__(self, hidden_size, char_embedding_size=50, target_vocab=None):
@@ -27,10 +34,16 @@ class CharDecoder(nn.Module):
         ### Hint: - Use target_vocab.char2id to access the character vocabulary for the target language.
         ###       - Set the padding_idx argument of the embedding matrix.
         ###       - Create a new Embedding layer. Do not reuse embeddings created in Part 1 of this assignment.
-        
+
+        super(CharDecoder, self).__init__()
+        self.hidden_size = hidden_size
+        self.target_vocab = target_vocab
+        self.padding_idx = self.target_vocab.char2id['<pad>']
+        self.charDecoder = nn.LSTM(char_embedding_size, hidden_size)
+        self.char_output_projection = nn.Linear(hidden_size, len(self.target_vocab.char2id))
+        self.decoderCharEmb = nn.Embedding(len(self.target_vocab.char2id), char_embedding_size, padding_idx=self.padding_idx)
 
         ### END YOUR CODE
-
 
     
     def forward(self, input, dec_hidden=None):
@@ -44,8 +57,10 @@ class CharDecoder(nn.Module):
         """
         ### YOUR CODE HERE for part 2b
         ### TODO - Implement the forward pass of the character decoder.
-        
-        
+        X = self.decoderCharEmb(input) # (word_length, batch, char_emb)
+        h_t, dec_hidden = self.charDecoder(X, dec_hidden)
+        scores = self.char_output_projection(h_t) # h_t (word_length, batch, hidden_size)
+        return scores, dec_hidden
         ### END YOUR CODE 
 
 
@@ -62,8 +77,11 @@ class CharDecoder(nn.Module):
         ###
         ### Hint: - Make sure padding characters do not contribute to the cross-entropy loss.
         ###       - char_sequence corresponds to the sequence x_1 ... x_{n+1} from the handout (e.g., <START>,m,u,s,i,c,<END>).
-
-
+        scores, dec_hidden = self.forward(char_sequence[:-1], dec_hidden)
+        p_t = F.softmax(scores, dim=2) # (length, batch, self.vocab_size)
+        loss = nn.CrossEntropyLoss(ignore_index=self.padding_idx, reduction='sum')(p_t.permute(1, 2, 0), char_sequence[1:].t())
+        logger.debug('the shape of loss is: {shape}'.format(shape=loss))
+        return loss
         ### END YOUR CODE
 
     def decode_greedy(self, initialStates, device, max_length=21):
@@ -83,7 +101,29 @@ class CharDecoder(nn.Module):
         ###      - Use torch.tensor(..., device=device) to turn a list of character indices into a tensor.
         ###      - We use curly brackets as start-of-word and end-of-word characters. That is, use the character '{' for <START> and '}' for <END>.
         ###        Their indices are self.target_vocab.start_of_word and self.target_vocab.end_of_word, respectively.
-        
+        output_words_id = []
+        # 初始化 batch 个 start
+        current_char = torch.zeros(1, initialStates[0].size()[1], dtype=torch.int64, device=device) # (1, batch)
+        current_char.fill_(self.target_vocab.start_of_word)
+        logger.debug('dtype of current char is: {dtype}'.format(dtype=current_char.dtype))
+        dec_hidden = initialStates
+
+        # 按时间步解码
+        for _ in range(max_length):
+            scores, dec_hidden = self.forward(current_char, dec_hidden)
+            p_t = F.softmax(scores, dim=2)
+            current_char = p_t.argmax(dim=2)
+            output_words_id.append(current_char.squeeze(0).tolist())
+
+        # 将索引转换为字符并拼接成单词
+        output_words = []
+        for words_idx in zip(*output_words_id):
+            tmp = []
+            for i in words_idx:
+                if i == self.target_vocab.end_of_word: break
+                tmp.append(self.target_vocab.id2char[i])
+            output_words.append(''.join(tmp))
+        return output_words
         
         ### END YOUR CODE
 
